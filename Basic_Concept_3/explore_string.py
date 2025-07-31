44ce74700faa19f94d5c7c71b5e1b9e505a329ec
ashrafulislampro <ashrafulislam1659@gmail.com>
2025-07-31 09:57:25 +0600

    explore string with python

---
+++ Basic_Concept_3/explore_string.py
@@ -0,0 +1,14 @@
+name = "John\'s Doe"
+name1 = "Ashraful Islam"
+name2 = """
+Shakib Khan 
+Number one
+"""
+# name[0] = 'A'
+print(name)
+# print(name1)
+# print(name2)
+
+# string is a sequence of characters
+# mutable is changeable 
+# immutable is unchangeable
